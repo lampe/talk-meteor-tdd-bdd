433722e8b5e5cadbc908f3d31390ef18db54f6a9
Michael Lazarski <michael@lazarski.me>
2016-03-17 13:32:27 +0100

    finished code for step1

--- server/modules/rockPaperScissors/rockPaperScissors.js
+++ server/modules/rockPaperScissors/rockPaperScissors.js
@@ -1,1 +1,7 @@
 RoPaSc = new Restivus({prettyJson: true});
+RoPaSc.allowedMoves = ['rock', 'paper', 'scissors', 'well'];
+RoPaSc.winningMoves = {
+  rock: 'scissors',
+  paper: 'rock',
+  scissors: 'paper'
+};
